475558d11d31eebdfef82dd518d8145e245e77ec
Kris Santos <kris.santos101@gmail.com>
2023-05-01 03:48:21 -0600

    creating constant file

---
+++ constants.js
@@ -0,0 +1,13 @@
+const IP = "172.24.5.65";
+const PORT = 50541;
+const connected = "Connected";
+const player = "Name: KMS";
+const quit = "\u0003";
+
+module.exports = {
+  IP,
+  PORT,
+  connected,
+  player,
+  quit
+};
