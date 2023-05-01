11edc5e1ae6639010cb5154a47ff0ac8649cfc51
Kris Santos <kris.santos101@gmail.com>
2023-05-01 00:36:52 -0600

    set up interface to handle user input from stdin

--- play.js
+++ play.js
@@ -2,3 +2,23 @@ const {connect} = require("./client");
 
 console.log("Connecting ...");
 connect();
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  // register event listener
+  stdin.on("data", handleUserInput); 
+  return stdin;
+  };
+  // detect ctrl + c to exit
+  const handleUserInput = function (key) {
+  if (key === '\u0003') {
+  process.exit();
+  }
+  };
+  
+  setupInput();
